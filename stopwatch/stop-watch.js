9b6bd771f08636f05b1d50580f47fcee7951793a
Kdash166 <blacknoir_619@hotmail.co.uk>
2023-11-01 19:55:33 +0000

    finished stopwatch

--- stopwatch/stop-watch.js
+++ stopwatch/stop-watch.js
@@ -1,44 +1,51 @@
+let miliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let timerID;
-let isRunning = false; // Variable to track whether the timer is running
+let isRunning = false;
 
 function updateDisplay() {
+  const formattedMiliSeconds = miliseconds < 10 ? `0${miliseconds}` : miliseconds;
   const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
   const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-  document.querySelector('.js-seconds').innerHTML = formattedSeconds;
-  document.querySelector('.js-minutes').innerHTML = formattedMinutes;
+  document.querySelector(".js-seconds").innerHTML = formattedSeconds;
+  document.querySelector(".js-minutes").innerHTML = formattedMinutes;
+  document.querySelector(".js-miliseconds").innerHTML = formattedMiliSeconds;
 }
 
-document.querySelector('.start').addEventListener('click', function () {
-  if (!isRunning) { // Check if the timer is not already running
+document.querySelector(".start").addEventListener("click", function () {
+  if (!isRunning) {
     timerID = setInterval(function () {
-      if (seconds === 60) {
+      if (miliseconds === 99) {
+        miliseconds = 0;
+        seconds++;
+      } else {
+        miliseconds++;
+      }
+      if (seconds === 59) {
         seconds = 0;
         minutes++;
-      } else {
-        seconds++;
       }
       updateDisplay();
-    }, 1000);
-    
-    isRunning = true; // Set the timer state to running
+    }, 10);
+
+    isRunning = true;
   }
 });
 
-document.querySelector('.stop').addEventListener('click', function () {
+document.querySelector(".stop").addEventListener("click", function () {
   clearInterval(timerID);
-  isRunning = false; // Set the timer state to not running
+  isRunning = false;
 });
 
-document.querySelector('.reset').addEventListener('click', function () {
+document.querySelector(".reset").addEventListener("click", function () {
   clearInterval(timerID);
   minutes = 0;
   seconds = 0;
+  miliseconds = 0;
   updateDisplay();
-  isRunning = false; // Set the timer state to not running
+  isRunning = false;
 });
 
 updateDisplay();
-
